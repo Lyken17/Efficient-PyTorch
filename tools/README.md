8d46008f49a8ee36725584750b92fa7bbb08bcfc
lyken17 <lykensyu@gmail.com>
2018-04-01 01:56:48 -0700

    Folder 2 lmdb

---
+++ tools/README.md
@@ -0,0 +1,30 @@
+## LMDB 
+LMDB is a json-like, but binarized key-value storage. In my design, the format of converted LMDB is defined as follow.
+
+key | value 
+--- | ---
+img-id1 | (jpeg_raw1, label1)
+img-id2 | (jpeg_raw2, label2)
+img-id3 | (jpeg_raw3, label3)
+... | ...
+img-idn | (jpeg_rawn, labeln)
+`__keys__` | [img-id1, img-id2, ... img-idn]
+`__len__` | n
+
+As for details of reading/writing, please refer to [code](folder2lmdb.py).
+
+
+## Convert `ImageFolder` to `LMDB`
+```bash
+python folder2lmdb.py --folder ~/torch_data/ --name train
+```
+
+## ImageFolderLMDB
+The usage of `ImageFolderLMDB` is identical to `torchvision.datasets`. 
+
+```python
+import ImageFolderLMDB
+from torch.utils.data import DataLoader
+dst = ImageFolderLMDB(path, transform, target_transform)
+loader = DataLoader(dst, batch_size=64)
+```
---
+++ tools/folder2lmdb.py
@@ -0,0 +1,170 @@
+import os
+import os.path as osp
+import os, sys
+import os.path as osp
+from PIL import Image
+import six
+import string
+
+import lmdb
+import pickle
+import msgpack
+import tqdm
+import pyarrow as pa
+
+import torch
+import torch.utils.data as data
+from torch.utils.data import DataLoader
+from torchvision.transforms import transforms
+from torchvision.datasets import ImageFolder
+from torchvision import transforms, datasets
+
+
+class ImageFolderLMDB(data.Dataset):
+    def __init__(self, db_path, transform=None, target_transform=None):
+        self.db_path = db_path
+        self.env = lmdb.open(db_path, subdir=osp.isdir(db_path),
+                             readonly=True, lock=False,
+                             readahead=False, meminit=False)
+        with self.env.begin(write=False) as txn:
+            # self.length = txn.stat()['entries'] - 1
+            self.length = txn.get(b'__len__')
+            self.keys = msgpack.loads(txn.get(b'__keys__'))
+
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __getitem__(self, index):
+        img, target = None, None
+        env = self.env
+        with env.begin(write=False) as txn:
+            byteflow = txn.get(self.keys[index])
+        unpacked = msgpack.loads(byteflow)
+
+        # load image
+        imgbuf = unpacked[0]
+        buf = six.BytesIO()
+        buf.write(imgbuf)
+        buf.seek(0)
+        img = Image.open(buf).convert('RGB')
+
+        # load label
+        target = unpacked[1]
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target
+
+    def __len__(self):
+        return self.length
+
+    def __repr__(self):
+        return self.__class__.__name__ + ' (' + self.db_path + ')'
+
+
+class ImageFolderLMDB_old(data.Dataset):
+    def __init__(self, db_path, transform=None, target_transform=None):
+        import lmdb
+        self.db_path = db_path
+        self.env = lmdb.open(db_path, subdir=osp.isdir(db_path),
+                             readonly=True, lock=False,
+                             readahead=False, meminit=False)
+        with self.env.begin(write=False) as txn:
+            self.length = txn.stat()['entries'] - 1
+            self.keys = msgpack.loads(txn.get(b'__keys__'))
+        # cache_file = '_cache_' + db_path.replace('/', '_')
+        # if os.path.isfile(cache_file):
+        #     self.keys = pickle.load(open(cache_file, "rb"))
+        # else:
+        #     with self.env.begin(write=False) as txn:
+        #         self.keys = [key for key, _ in txn.cursor()]
+        #     pickle.dump(self.keys, open(cache_file, "wb"))
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __getitem__(self, index):
+        img, target = None, None
+        env = self.env
+        with env.begin(write=False) as txn:
+            byteflow = txn.get(self.keys[index])
+        unpacked = msgpack.loads(byteflow)
+        imgbuf = unpacked[0][b'data']
+        buf = six.BytesIO()
+        buf.write(imgbuf)
+        buf.seek(0)
+        img = Image.open(buf).convert('RGB')
+        target = unpacked[1]
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target
+
+    def __len__(self):
+        return self.length
+
+    def __repr__(self):
+        return self.__class__.__name__ + ' (' + self.db_path + ')'
+
+
+def raw_reader(path):
+    with open(path, 'rb') as f:
+        bin_data = f.read()
+    return bin_data
+
+
+def dumps_pyarrow(obj):
+    """
+    Serialize an object.
+
+    Returns:
+        Implementation-dependent bytes-like object
+    """
+    return pa.serialize(obj).to_buffer()
+
+
+def folder2lmdb(dpath, name="train", write_frequency=5000):
+    directory = osp.expanduser(osp.join(dpath, name))
+    print("Loading dataset from %s" % directory)
+    dataset = ImageFolder(directory, loader=raw_reader)
+    data_loader = DataLoader(dataset, num_workers=16, collate_fn=lambda x: x)
+
+    lmdb_path = osp.join(dpath, "%s.lmdb" % name)
+    isdir = os.path.isdir(lmdb_path)
+
+    print("Generate LMDB to %s" % lmdb_path)
+    db = lmdb.open(lmdb_path, subdir=isdir,
+                   map_size=1099511627776 * 2, readonly=False,
+                   meminit=False, map_async=True)
+
+    txn = db.begin(write=True)
+    for idx, data in enumerate(data_loader):
+        # print(type(data), data)
+        image, label = data[0]
+        txn.put(u'{}'.format(idx).encode('ascii'), dumps_pyarrow((image, label)))
+        if idx % write_frequency == 0:
+            print("[%d/%d]" % (idx, len(data_loader)))
+            txn.commit()
+            txn = db.begin(write=True)
+
+    # finish iterating through dataset
+    txn.commit()
+    keys = [u'{}'.format(k).encode('ascii') for k in range(idx + 1)]
+    with db.begin(write=True) as txn:
+        txn.put(b'__keys__', dumps_pyarrow(keys))
+        txn.put(b'__len__', dumps_pyarrow(len(keys)))
+
+    print("Flushing database ...")
+    db.sync()
+    db.close()
+
+
+if __name__ == "__main__":
+    folder2lmdb("/home/ligeng/torch_data/places365")
